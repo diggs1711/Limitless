0dfe8a47102d80c777626f064107cb3da60d9c24
Higgins <dahiggins@deloitte.ie>
2016-11-28 15:52:49 +0000

    HomeworK final

--- homework.js
+++ homework.js
@@ -1,27 +1,38 @@
-var arr = []
-var count = 0;
-arrayLength = 365;
+//Assignment 1
+//Same birtday problem
 
-arr = createArray(arrayLength);
-var randomElement = arr[getRandom()];
-findElement(randomElement);
+var assignmentOne = function() {
+    var arr = [];
+    var count = 0;
+    var max = 365;
 
-function findElement(a) {
-    while (randomElement !== getRandom()) {
-        count++;
+    var arr = createArray(max);
+    var count = matchPeopleWithSameBirthday();
+
+    var answer = document.getElementById("answer");
+    answer.innerHTML = count;
+
+    function matchPeopleWithSameBirthday() {
+        var birthdays = [];
+        for (var i = 0;; i++) {
+            var randomElement = arr[getRandom()];
+            if (birthdays.indexOf(randomElement) === -1) {
+                birthdays.push(randomElement);
+            } else {
+                return i;
+            }
+        }
+    }
+
+    function getRandom() {
+        return Math.floor((Math.random() * max) + 1);
+    }
+
+    function createArray(len) {
+        var arr = []
+        for (var i = 0; i < len; i++) {
+            arr.push(i + 1);
+        }
+        return arr;
     }
 }
-
-function getRandom() {
-    return Math.floor((Math.random() * arrayLength)) + 1;
-}
-
-function createArray(len) {
-    var arr = []
-    for (var i = 0; i < len; i++) {
-        arr.push(i + 1);
-    }
-    return arr;
-}
-
-console.log("It took " + count + " random calls to find the element " + randomElement);
