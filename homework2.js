1c4e1fa8cd270de6ad12dd6734aa7a0f236c909f
David <davidhiggins1712@gmail.com>
2016-12-04 18:50:43 +0000

    h2

--- homework2.js
+++ homework2.js
@@ -16,10 +16,36 @@ function checkIfPalindrome(str) {
 	}
 }
 
+//3.Add word to string if word not in string
+function addWordIfNotPresent(string, word) {
+	string = string.split(' ');
+
+	if(!(isWordPresent(string, word).length === 0)) {
+		return flattenArray(string, " ");
+	} else {
+		string.push(word);
+		var flat = flattenArray(string, " ");
+		return flat
+	}
+}
+
+function isWordPresent(string, word) {
+	var filter = string.filter(function(w) {
+		return w === word;
+	});
+	return filter;
+}
+
+//4. Remove a all instances of a word from a string
+function removeWordFromString(string, word) {
+	var r = new RegExp('\\b' + word + '\\b', 'g');
+	string = string.replace(r, "").trim().replace(/\s+/g, ' ');
+	return string;
+}
 
 (function(){
 	var arr = [1,2,3, [2,54,98, [43,9]],2,3];
-	falt = flattenArray(arr, ",");
+	flat = flattenArray(arr, ",");
 	console.log(flat);
 
 	var palindrome = "hannah";
@@ -27,5 +53,12 @@ function checkIfPalindrome(str) {
 
 	checkIfPalindrome(palindrome);
 	checkIfPalindrome(not);
+
+	var result = addWordIfNotPresent("hello foo bar", "bar")
+	console.log(result)
+
+	var str = 'foo bar bar foobar foo    hello    bar    foo   ';
+	var result = removeWordFromString(str, "bar");
+	console.log(result);
 
 })();
