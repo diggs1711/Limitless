3b436df0ef727d949694ae6c31ea8c74a4a5c2f7
David <davidhiggins1712@gmail.com>
2016-12-28 20:02:21 +0000

    xmas

---
+++ xmas special/xmasSpecial.js
@@ -0,0 +1,6 @@
+var data = [
+    {"Id":"1", "Name":"abc", "parent":""},  
+    {"Id":"2", "Name":"abc", "parent":"1"},
+    {"Id":"3", "Name":"abc", "parent":"2"},
+    {"Id":"4", "Name":"abc", "parent":"2"}
+];
