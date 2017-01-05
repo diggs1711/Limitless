2d2f0c30af1f35af641d7df9a57bd2f1343f993d
Higgins <dahiggins@deloitte.ie>
2017-01-05 10:08:46 +0000

    xmas

--- xmas special/xmasSpecial.js
+++ xmas special/xmasSpecial.js
@@ -34,6 +34,10 @@
             "Id": "4",
             "Name": "abc",
             "parent": "2"
+        }, {
+            "Id": "5",
+            "Name": "abc",
+            "parent": "3"
         }];
 
         tree = document.getElementsByClassName('tree')[0];
@@ -45,32 +49,35 @@
     }
 
     function createNodeMap() {
-      for (var i = 0; i < lengthOfData; i++) {
-          node = data[i];
-          map[node.Id] = node;
-          map[node.Id].children = [];
-      }
+        for (var i = 0; i < lengthOfData; i++) {
+            node = data[i];
+            map[node.Id] = node;
+            map[node.Id].children = [];
+        }
     }
 
     function createTree() {
-      for (var id in map) {
-          if (map.hasOwnProperty(id)) {
-              mappedElem = map[id];
+        for (var id in map) {
+            if (map.hasOwnProperty(id)) {
+                mappedElem = map[id];
 
-              if (mappedElem.parent !== "") {
-                  map[mappedElem['parent']]['children'].push(mappedElem);
-              } else {
+                if (!isRootNode()) {
+                    map[mappedElem['parent']]['children'].push(mappedElem);
+                } else {
+                    roots.push(mappedElem);
+                }
+            }
+        }
+    }
 
-                  roots.push(mappedElem);
-              }
-          }
-      }
+    function isRootNode() {
+        return mappedElem.parent === "";
     }
 
     function displayTree() {
-      setTimeout(function() {
-          tree.innerHTML = JSON.stringify(roots, undefined, 2);
-      }, 3000);
+        setTimeout(function() {
+            tree.innerHTML = JSON.stringify(roots, undefined, 2);
+        }, 3000);
     }
 
     start();
