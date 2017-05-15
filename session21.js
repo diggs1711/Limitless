e9cdcd54df14b785b231131f4cd06939f5dc5c42
Higgins <dahiggins@deloitte.ie>
2017-05-15 22:24:07 +0100

    m

---
+++ session21.js
@@ -0,0 +1,21 @@
+
+var listItem = function() {
+  var o = {
+    ele: null,
+    iconEle: null,
+    numEle: null,
+    template: [
+      '<div class="item__icon"></div>',
+      '<div class="item__num-of-items"></div>'
+    ].join(''),
+    data: null,
+
+
+    init: function() {},
+
+    initEvent: function() {
+      this.ele = document.createElement('div');
+      this.ele.
+    }
+  }
+}
