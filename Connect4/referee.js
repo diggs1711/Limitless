e299df2689b34bc65d0158e7591961faf30d945a
Higgins <dahiggins@deloitte.ie>
2017-05-16 11:10:32 +0100

    m

--- Connect4/referee.js
+++ Connect4/referee.js
@@ -1,10 +1,10 @@
 (function() {
 
     var referee = function(board) {
-      this.redPlayerScore = 0;
-      this.yellowPlayerScore = 0
-      this.boardToReferee = board;
-      this.gameOver = false;
+        this.redPlayerScore = 0;
+        this.yellowPlayerScore = 0
+        this.boardToReferee = board;
+        this.gameOver = false;
     };
 
     referee.prototype.checkGameStatus = function(player, cell) {
@@ -15,23 +15,34 @@
     referee.prototype.checkVertical = function(player, cell) {
         var c = player.colour;
 
-        for(i = 0; i < 5; i++) {
-          console.log(this.boardToReferee.valueArray);
-          for(j=0; j< 8; j++) {
-              if(this.boardToReferee.valueArray[i][j] == c &&
-                this.boardToReferee.valueArray[i + 1][j] == c &&
-                this.boardToReferee.valueArray[i + 2][j] == c &&
-              this.boardToReferee.valueArray[i +  3][j] == c) {
-                alert("player " + player.colour + " wins!");
-                this.gameOver = true;
-              }
-          }
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 8; j++) {
+                if (this.boardToReferee.valueArray[i][j] == c &&
+                    this.boardToReferee.valueArray[i + 1][j] == c &&
+                    this.boardToReferee.valueArray[i + 2][j] == c &&
+                    this.boardToReferee.valueArray[i + 3][j] == c) {
+                    alert("player " + player.colour + " wins!");
+                    this.gameOver = true;
+                }
+            }
         }
 
     };
 
     referee.prototype.checkHorizontal = function(player, cell) {
-
+      var c = player.colour;
+      
+      for (i = 0; i < 5; i++) {
+          for (j = 0; j < 8; j++) {
+              if (this.boardToReferee.valueArray[i][j] == c &&
+                  this.boardToReferee.valueArray[i + 1][j] == c &&
+                  this.boardToReferee.valueArray[i + 2][j] == c &&
+                  this.boardToReferee.valueArray[i + 3][j] == c) {
+                  alert("player " + player.colour + " wins!");
+                  this.gameOver = true;
+              }
+          }
+      }
     };
 
 
